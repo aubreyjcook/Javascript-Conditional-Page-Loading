42ea6ddd9cc68127ce31a11ed554b9e4ed8dc5dd
aubreyjcook <aubreyjcook@gmail.com>
2018-04-25 20:52:11 -0500

    basic method call on document loading

--- src/js/conditionalPageLoading.js
+++ src/js/conditionalPageLoading.js
@@ -1,2 +1,13 @@
-//get the current page
-var currentDocument = location.href.split("/").slice(-1);
+document.onload = conditionalPageLoading();
+
+function conditionalPageLoading(){
+  //appends to all pages
+  $('head').append('<link rel="stylesheet" href="css/main.css">');
+
+  //get the current page
+  var currentDocument = location.href.split("/").slice(-1);
+
+  if (currentDocument === "index.html"){
+    $('head').append('<link rel="stylesheet" href="css/index.css">');
+  }
+}

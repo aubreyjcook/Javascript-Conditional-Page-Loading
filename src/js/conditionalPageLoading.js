39b2b65ef5b741a306a6f8fd2988e7528a529d58
aubreyjcook <aubreyjcook@gmail.com>
2018-05-09 19:10:25 -0500

    loadRegularCSS method created - basic functionality

--- src/js/conditionalPageLoading.js
+++ src/js/conditionalPageLoading.js
@@ -4,7 +4,22 @@ function conditionalPageLoading() {
   // TODO: create functions for loading head information for:
   //
 
-  // TODO: create conditional logic to check if main/normalize.css files are present in their usual location and load them if they exist
+  // TODO: turn the below code into a method, use reg ex string parsing to make it load the file's directory location into a var, and conditionally check each critical css file
+  loadRegularCSS();
+
+  /*
+  $.ajax({
+    url: 'http://localhost/javascript-conditional-page-loading/css/normalize.css',
+    type: 'HEAD',
+    error: function()
+    {
+      //file does not exist
+    },
+    success: function(){
+      //file exists
+      $('head').append('<link rel="stylesheet" href="css/normalize.css">');
+    }
+  });
 
   $.ajax({
     url: 'http://localhost/javascript-conditional-page-loading/css/main.css',
@@ -18,7 +33,7 @@ function conditionalPageLoading() {
       $('head').append('<link rel="stylesheet" href="css/main.css">');
     }
   });
-
+  */
 /*
   //universal css file appends to all pages
   $('head').append('<link rel="stylesheet" href="css/main.css">');
@@ -31,4 +46,24 @@ function conditionalPageLoading() {
   if (currentDocument.toString() !== '') {
     $('head').append('<link rel="stylesheet" href="css/' + currentDocument + '.css">');
   }
+}
+
+
+//methods
+function loadRegularCSS() {
+  var siteURL = document.location.origin + document.location.pathname;
+
+  $.ajax({
+    url: siteURL + 'css/normalize.css',
+    type: 'HEAD',
+    error: function()
+    {
+      //file does not exist
+    },
+    success: function(){
+      //file exists
+      $('head').append('<link rel="stylesheet" href="css/normalize.css">');
+    }
+  });
+  
 }
